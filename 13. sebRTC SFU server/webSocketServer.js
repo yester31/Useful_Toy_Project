a791835c9384131a771695c1316bc17b3acc3d42
yester31 <yester31@naver.com>
2021-07-31 00:53:05 +0900

    코드 정리

--- 13. sebRTC SFU server/webSocketServer.js
+++ 13. sebRTC SFU server/webSocketServer.js
@@ -2,18 +2,15 @@
 const funcs = require('./public/libs');  // 커스텀 라이브러리 불러오기
 const WebSocket = require('ws');         // 웹소켓 라이브러리
 const Hashtable = require('jshashtable');// 해시테이블 라이브러리
-const wshashtable = new Hashtable();     // 해시테이블 생성
-const streamtable = new Hashtable();     // 해시테이블 생성
-
-//global.wshashtable = wshashtable;     // 해시테이블 전역 변수화 시키기
-let wrtc = require('wrtc'); // webrtc 
+const wrtc = require('wrtc');            // webrtc 라이브러리 
+const wshashtable = new Hashtable();     // 웹소켓 정보 저장 해시테이블 생성
+const streamtable = new Hashtable();     // 스트림 저장 해시테이블 생성
 
 module.exports = (server) => {
   // 웹소켓 서버 객체 생성
   const wss = new WebSocket.Server({ server });
   // 클라이언트 접속할 경우 그 클라이언트와 연결된 소켓객체(ws) 생성 
   wss.on('connection', (ws, req) => {
-
     // 웹 소켓 연결 시 클라이언트 정보 수집
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     const port = req.headers['x-forwarded-port'] || req.socket.remotePort;
@@ -34,67 +31,63 @@ module.exports = (server) => {
     const configuration = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]} // stun 서버 주소
     var peerConnection = new wrtc.RTCPeerConnection(configuration); // RTCPeerConnection객체 생성
     let remoteStream = null;
-    //let localStream = null;
-    // 로컬 스트림을 track에 추가
     let localStream = null;
+
     if (wstype == 'sub' && streamtable.keys().length > 0){  
-      console.log('[add pub stream] track!!')
-      localStream = streamtable.get('pub');
+      console.log('[Downstream] send pub stream to sub client')
+      localStream = streamtable.get('pub'); // 전달할 스트림이 있다면
     }else{
-      localStream = new wrtc.MediaStream();
+      console.log('[Downstream] no pub stream')
+      localStream = new wrtc.MediaStream(); // 전달할 스트림이 없다면 빈 스트림 임시로 전달
     }
     
-    localStream.getTracks().forEach(track => { // upload
+     // sub 클라이언트에게 전달할 스트림을 track에 추가
+    localStream.getTracks().forEach(track => {
       console.log(localStream);
       peerConnection.addTrack(track, localStream);
     });  
-    // var remoteVideo = document.querySelector('#remoteVideo');
-    // 원격에서 전달되는 스트림 수신하기 위해 새로 추가된 track이 있는지 확인하는 리스너
+
+    // pub에서 전달 받은 스트림을 해시테이블에 저장
     if (wstype == 'pub'){
-      peerConnection.addEventListener('track', async (event) => {// download 
-        remoteStream = event.streams[0];   // 새로 발생된 track의 원격 스트림을 화면에 출력 
+      peerConnection.addEventListener('track', async (event) => {
+        remoteStream = event.streams[0];
         streamtable.put('pub', remoteStream);
-        console.log('[Listener] find track and get remote stream !!')
-        //console.log(event);
+        console.log('[Upstream] find track and get pub stream !!')
         console.log(remoteStream)
       });
     }
+
     // 로컬의 ICE 후보를 수집 하기 위해 icecandidate를 이벤트로 등록
     peerConnection.addEventListener('icecandidate', event => {
       if (event.candidate) {
         console.log('[Listener] find new icecandidate!!')
-        ws.send(JSON.stringify({optype : 'new-ice-candidate', icecandidate : event.candidate, 'wsid' : 'server'})); // 발견된 ICE를 원격 피어로 전달
+        ws.send(JSON.stringify({optype : 'new-ice-candidate', icecandidate : event.candidate, 'wsid' : 'server'})); // 발견된 ICE를 클라이언트 피어로 전달
       }else{
         console.log('[Listener] All ICE candidates have been sent');
       }
     });
+
     // 연결 완료 상황을 알기 위해 connectionstatechange 이벤트 등
     peerConnection.addEventListener('connectionstatechange', event => {
       if (event) {
         console.log(`[Listener] 연결 상태 :: ${peerConnection.connectionState}`) // 연결 상태 체크
       }
     });
-    async function getOffer(peerConnection){
-      console.log("4. [호출측] 정보 준비 offer")
-      const offer = await peerConnection.createOffer();   // 로컬에 대한 정보를 담은 객체 생성
-      await peerConnection.setLocalDescription(offer);    // 이 객체를 로컬 설정에 등록
-      //console.log(offer)
-      return offer
-    }
+
     async function treatOffer(peerConnection, offer){
-      console.log("7. [수신측] 전달 받은 offer 내용을 수신측의 원격 정보로 등록.")
+      console.log("[수신측] 전달 받은 offer 내용을 수신측의 원격 정보로 등록.")
       await peerConnection.setRemoteDescription(new wrtc.RTCSessionDescription(offer));// 전달 받은 호출 측 메타 정보를 이용하여 세션 객체로 생성하고 원격 설정 내용에 등록
       const answer = await peerConnection.createAnswer(); // 수신 측 피어에 대한 정보를 담은 객체 생성
       await peerConnection.setLocalDescription(answer);  // 수신 측 피어 로컬 정보에 위 객체를 등록
       return answer
     }
+
     async function treatAnswer(peerConnection, answer){
-      console.log("10. [호출측] 전달 받은 answer 내용을 호출측의 원격 정보로 등록")
+      console.log("[호출측] 전달 받은 answer 내용을 호출측의 원격 정보로 등록")
       //console.log(answer);
       const remoteDesc = await new wrtc.RTCSessionDescription(answer); // 전달 받은 수신측 메타 정보로 세션 객체로 생성
       await peerConnection.setRemoteDescription(remoteDesc);  // 수신 측 세션 객체를 피어의 원격의 설정 내용에 등록
     }
-
 
     // 클라이언트로부터 메시지 수신 시
     ws.on('message', (fmessage) => {  
@@ -128,24 +121,14 @@ module.exports = (server) => {
             if (key == msgjson.wsid ){
               treatOffer(peerConnection, msgjson.offer)
               .then(answer =>{
-                console.log("8. [수신측] 호출측으로 수신측의 peerConnection 정보가 담긴 answer 전달 ");
+                console.log("[수신측] 호출측으로 수신측의 peerConnection 정보가 담긴 answer 전달 ");
                 ws.send(JSON.stringify({'optype': 'answer' ,'answer': answer, 'wsid' : wsid}));  // 호출 측에 응답으로 수신측 피어에 대한 메타 정보를 전달
               })
               console.log(key + " 로 offer 전달")
             }
           })
           break;
-        case 'answer': 
-          console.log("answer 요청 :: ")
-          wshashtable.keys().forEach(key =>{
-            if (key == msgjson.wsid ){
-              treatAnswer(peerConnection, msgjson.answer).then(()=>{
-                console.log("11. [호출측] 수신받은 answer 처리 끝. :: ");
-              })
-              console.log(key + " 로 answer 전달")
-            }          
-          })
-          break;
+
         case 'new-ice-candidate': 
           console.log("new-ice-candidate 요청 :: ")
           wshashtable.keys().forEach(key =>{
